48452781d26dd7d7d8152baa7c83d03c154766e7
Tanner Babcock <babkock@protonmail.com>
2024-10-30 10:57:08 -0500

    Initial commit

---
+++ tests/cli_tests.rs
@@ -0,0 +1,28 @@
+#[test]
+fn cli_all_tests() {
+    trycmd::TestCases::new().case("tests/cmd/*_all.toml");
+}
+
+#[test]
+#[cfg(unix)]
+fn cli_unix_tests() {
+    trycmd::TestCases::new().case("tests/cmd/*_unix.toml");
+}
+
+#[test]
+#[cfg(windows)]
+fn cli_windows_tests() {
+    trycmd::TestCases::new().case("tests/cmd/*_windows.toml");
+}
+
+#[test]
+#[cfg(feature = "nix")]
+fn cli_nix_tests() {
+    trycmd::TestCases::new().case("tests/cmd/*_nix.toml");
+}
+
+#[test]
+#[cfg(feature = "nix-local")]
+fn cli_nix_local_tests() {
+    trycmd::TestCases::new().case("tests/cmd/*_nix_local.toml");
+}
